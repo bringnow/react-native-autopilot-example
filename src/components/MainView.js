13493b05c68b51cd8145c8c793ee54f818069247
Fabian Köster <fabian.koester@bringnow.com>
2016-06-08 14:13:01 +0200

    Show how to pop navigation stack by using back button on android

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Platform, BackAndroid } from 'react-native';
 import { connect } from 'react-redux';
 import { ExNavigator } from 'react-native-autopilot';
 import { mapRoute } from '../routes';
@@ -9,6 +10,27 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.mapRouteWithProps = (route) => mapRoute(this.props, route);
+  }
+
+  componentDidMount() {
+    if (Platform.OS === 'android' && !this.backButtonListener) {
+      this.backButtonListener = () => {
+        if (this.props.routes.length > 1) {
+          this.props.popNavigationRoute();
+          return true;
+        }
+
+        return false;
+      };
+      BackAndroid.addEventListener('hardwareBackPress', this.backButtonListener);
+    }
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === 'android') {
+      BackAndroid.removeEventListener(this.backButtonListener);
+      this.backButtonListener = null;
+    }
   }
 
   render() {
@@ -32,6 +54,7 @@ const mapStateToProps = (state) => ({
 MainView.propTypes = {
   routes: React.PropTypes.array.isRequired,
   setNavigationRoutes: React.PropTypes.func.isRequired,
+  popNavigationRoute: React.PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, actions)(MainView);
